cc367d9bab6132a113bc00dcf3a7ad87c54cce0e
Jesse <jesse_adkins@icloud.com>
2023-11-12 14:00:58 -0800

    refine js

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-let themeToggle = document.getElementById("theme-toggle");
-let themeIcon = document.getElementById("theme-icon");
-let generateBtn = document.getElementById("generate-btn");
-let resetBtn = document.getElementById("reset-btn");
-let copyBtnOne = document.getElementById("copy-btn-one");
-let copyBtnTwo = document.getElementById("copy-btn-two");
-let passwordOne = document.getElementById("password-one");
-let passwordTwo = document.getElementById("password-two");
+const themeToggle = document.getElementById("theme-toggle");
+const themeIcon = document.getElementById("theme-icon");
+const generateBtn = document.getElementById("generate-btn");
+const resetBtn = document.getElementById("reset-btn");
+const copyBtnOne = document.getElementById("copy-btn-one");
+const copyBtnTwo = document.getElementById("copy-btn-two");
+const passwordOne = document.getElementById("password-one");
+const passwordTwo = document.getElementById("password-two");
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let lengthBtn = document.querySelectorAll(".length-btn");
+const lengthBtn = document.querySelectorAll(".length-btn");
+
+// Default password
 let lengthValue = 15;
 
 
-function reset(value = 15){
-    // Remove "checked" class from all elements
+function resetValues(value = 15){
+    // Loop through length buttons and toggle "checked" class based on button data-value and value passed
     for (let i = 0; i < lengthBtn.length; i++) {
             lengthBtn[i].classList.toggle("checked", parseInt(lengthBtn[i].dataset.value) === value);
     }
-    lengthValue = 15;
+    lengthValue = value;
     passwordOne.textContent = "";
     passwordTwo.textContent = "";
     copyBtnOne.classList.remove('copied');
     copyBtnTwo.classList.remove('copied');
 }
 
+// Add event click to each button. When clicked, pass button data-value to resetValues function to toggle clicked button styles, update length value and reset password container values
 for(let i = 0; i < lengthBtn.length; i++){
     lengthBtn[i].addEventListener('click', ()=>{
-        reset(parseInt(lengthBtn[i].dataset.value));
-        // Add "checked" class to the clicked element and update lengthValue with selected length
-        lengthBtn[i].classList.add('checked');
-        lengthValue = parseInt(lengthBtn[i].dataset.value);
+        resetValues(parseInt(lengthBtn[i].dataset.value));
     });
 }
 
-// Function uses random index on characters array to generate and return password that is 15 characters long
+// Function uses random index on characters array to generate and return password that is (default) 15 characters long
 function generatePassword(value = 15) {
     let randomIndex = "";
     let password = "";
@@ -44,20 +44,19 @@ function generatePassword(value = 15) {
     return password;
 }
 
+// function copies password to system clipboard
 function copyClick(password, btn) {
     navigator.clipboard.writeText(password.innerText)
     btn.classList.add('copied');
 }
 
-
-// Display two passwords on button click
 generateBtn.addEventListener('click', ()=>{
     passwordOne.textContent = generatePassword(lengthValue);
     passwordTwo.textContent = generatePassword(lengthValue);
 });
 
 resetBtn.addEventListener('click', ()=>{
-    reset();
+    resetValues();
 });
 
 copyBtnOne.addEventListener('click', () => {
